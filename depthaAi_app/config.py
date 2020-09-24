32555ec17e2c5fe2db8ac7da6b5e98bad0717c49
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2020-09-24 18:29:13 +0530

    depthai log and model config

---
+++ depthaAi_app/config.py
@@ -0,0 +1,23 @@
+import logging
+import os
+import sys
+from pathlib import Path
+from concurrent_log_handler import ConcurrentRotatingFileHandler
+
+# Log configuration
+root = logging.getLogger()
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+logfile = os.path.abspath("camera.log")
+
+# Rotate log after reaching 512K, keep 5 old copies.
+rotateHandler = ConcurrentRotatingFileHandler(logfile, "a", 512*1024, 5)
+rotateHandler.setFormatter(formatter)
+root.addHandler(rotateHandler)
+root.setLevel(logging.INFO)
+root.info("Logging system initialized, kept in file {}...".format(logfile))
+
+# Model path
+model = str(Path('models/').resolve())
